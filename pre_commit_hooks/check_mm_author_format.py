09db64561010661637a4bd51f37a3cbe06480644
marko <marko.martonosi@gmail.com>
2019-07-25 17:46:42 +0200

    Minor changes

--- pre_commit_hooks/check_mm_author_format.py
+++ pre_commit_hooks/check_mm_author_format.py
@@ -5,7 +5,6 @@ import argparse
 from typing import Optional
 from typing import Sequence
 
-
 def main(argv=None):
     print("Error in commit. Returning 1!")
     # parser = argparse.ArgumentParser()
@@ -15,6 +14,10 @@ def main(argv=None):
     print("new print")
     print(os.path.dirname(os.path.abspath(__file__)))
     print("working direcory>>", os.getcwd())
+    with open(".gitignore") as f:
+        data = f.read()
+        import pdb
+        pdb.set_trace()
     retval = 0
 
     retval = 1
