644bb0d9b9d7a9a61dbff9c009218c03aa5cd839
marko <marko.martonosi@gmail.com>
2019-07-26 09:33:57 +0200

    Chagnes

--- pre_commit_hooks/check_mm_author_format.py
+++ pre_commit_hooks/check_mm_author_format.py
@@ -12,13 +12,15 @@ def main(argv=None):
 
     print("---------------------")
     print("new print")
-    print(os.path.dirname(os.path.abspath(__file__)))
-    print("working direcory>>", os.getcwd())
+    print(os.path.dirname(os.path.abspath(__file__))) # change to this
+    print("working direcory>>", os.getcwd()) # from this
     git_config_file = os.path.join(os.getcwd(), ".git", "config")
     with open(git_config_file) as f:
         data = f.read()
-        import pdb
-        pdb.set_trace()
+        user_data = data.split("[user]")[1]
+        user_email = user_data.split("email = ")[1]
+        print(user_email)
+
     retval = 0
 
     retval = 1
