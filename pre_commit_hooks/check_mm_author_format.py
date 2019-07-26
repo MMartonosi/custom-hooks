25ee8efdc13809277c4a63f6bfc48f3568dfa29f
marko <marko.martonosi@gmail.com>
2019-07-26 10:22:19 +0200

    Final change

--- pre_commit_hooks/check_mm_author_format.py
+++ pre_commit_hooks/check_mm_author_format.py
@@ -1,29 +1,18 @@
-
-import sys
 import os
-import argparse
-from typing import Optional
-from typing import Sequence
+
 
 def main(argv=None):
-    print("Error in commit. Returning 1!")
-    # parser = argparse.ArgumentParser()
-    # args = parser.parse_args(argv)
-
-    print("---------------------")
-    print("new print")
-    print(os.path.dirname(os.path.abspath(__file__))) # change to this
-    print("working direcory>>", os.getcwd()) # from this
+    retval = 0
     git_config_file = os.path.join(os.getcwd(), ".git", "config")
     with open(git_config_file) as f:
         data = f.read()
         user_data = data.split("[user]")[1]
         user_email = user_data.split("email = ")[1]
-        print(user_email)
+        if not "@mediamonks.com" in user_email:
+            print("Email address used in this git repo doesn't "
+                  "seem to have mediamonks domain")
+            retval = 1
 
-    retval = 0
-
-    retval = 1
     return retval
 
 
