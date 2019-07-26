5a9154ef50f92ace04d58f7e25ea5a76630fce5e
MMartonosi <46603570+MMartonosi@users.noreply.github.com>
2019-07-26 10:11:02 +0200

    Change

--- pre_commit_hooks/check_mm_author_format.py
+++ pre_commit_hooks/check_mm_author_format.py
@@ -1,16 +1,10 @@
-
-import sys
 import os
-import argparse
-from typing import Optional
-from typing import Sequence
+
 
 def main(argv=None):
     retval = 0
-    print("---------------------")
-    print("new print")
     print("working direcory>>", os.getcwd())  # from this
-    print(os.path.dirname(os.path.abspath(__file__))) # change to this
+    print(os.path.dirname(os.path.abspath(__file__)))  # change to this
 
     git_config_file = os.path.join(os.getcwd(), ".git", "config")
     with open(git_config_file) as f:
@@ -18,9 +12,10 @@ def main(argv=None):
         user_data = data.split("[user]")[1]
         user_email = user_data.split("email = ")[1]
         if not "@mediamonks.com" in user_email:
-            print("Email address used in this git repo doesn't seem to have mediamonks domain")
+            print("Email address used in this git repo doesn't "
+                  "seem to have mediamonks domain")
             retval = 1
-    
+
     return retval
 
 
